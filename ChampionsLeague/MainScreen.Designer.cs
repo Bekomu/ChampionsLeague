58081a97b96dc21120b7f454c2f9f7b2ef483240
Berkay <bozisik93@gmail.com>
2022-04-18 14:14:40 +0300

    main screen size adjustment is disabled.

--- ChampionsLeague/MainScreen.Designer.cs
+++ ChampionsLeague/MainScreen.Designer.cs
@@ -140,6 +140,8 @@
             this.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.MainMenuStrip = this.menuStrip1;
             this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(696, 469);
             this.MinimumSize = new System.Drawing.Size(696, 469);
             this.Name = "MainScreen";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
